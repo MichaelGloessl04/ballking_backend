91d0666de3a257b7fe31fc509e932ed55b5f680c
MichaelGloessl04 <gloess190117@sr.htlweiz.at>
2024-04-03 15:36:34 +0100

    disable cors

--- api/main.py
+++ api/main.py
@@ -39,15 +39,9 @@ app = FastAPI(lifespan=lifespan,
                     'showCommonExtensions': True,
                 })
 
-origins = [
-    "http://localhost",
-    "http://localhost:5000",
-    "http://localhost:5000/",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
